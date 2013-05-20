8e2cece4feb6bf14d9ea8d7bc712f9499581e785
Scott Nelson <scott@scttnlsn.com>
2013-05-20 17:40:34 -0400

    Add client auth

--- app/controllers/clients.js
+++ app/controllers/clients.js
@@ -3,6 +3,12 @@ var Client = require('../models/client');
 
 var app = module.exports = express();
 
+app.use(function (req, res, next) {
+    var token = req.header('X-Token');
+    if (token !== process.env.AUTH_TOKEN) return res.json({ error: 'Unauthorized' }, 401);
+    next();
+});
+
 app.post('/', function (req, res, next) {
     Client.create(req.body, function (err, client) {
         if (err) return next(err);
